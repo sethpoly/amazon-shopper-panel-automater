2c74ca415d23751c1a550d4b4639ff2a61735096
sethpoly <emailspolyniak@gmail.com>
2021-03-18 19:58:58 -0400

    Setup main file with commented logic

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print()
+# Loop through all emails starting from most recent
+# Using receipt classifier, determine if is valid receipt
+# Send email to amazon shopper panel email adress if valid
+# Continue until 15 successful emails sent
+def check_mail():
+    print('Checking mail for receipts...')
