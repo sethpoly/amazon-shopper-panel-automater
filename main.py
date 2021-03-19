d0e4076d4dc489016e5c3cb923cbe607c132e62e
sethpoly <emailspolyniak@gmail.com>
2021-03-19 10:28:41 -0400

    Continue planning structure of main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Loop through all emails starting from most recent
 # Using receipt classifier, determine if is valid receipt
-# Send email to amazon shopper panel email adress if valid
+# Send email to amazon shopper panel email address if valid
 # Continue until 15 successful emails sent
 def check_mail():
     print('Checking mail for receipts...')
+
+
+# If determined as receipt, forward email to amazon {use correct 'from' address}
+def forward_email():
+    print('Forwarding email to receipts@panel.amazon.com...')
