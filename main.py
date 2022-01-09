342498b98336777d03385384676c81a47d63ae20
sethpoly <emailspolyniak@gmail.com>
2022-01-09 11:43:49 -0500

    decouple imap manager

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from email import message
 from email_parser import EmailParser
 from classifier import Classifier
+from imap_manager import ImapManager
 from spreadsheet import Spreadsheet
 import email
-import imaplib
 import os
-import time
-import traceback
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -16,31 +13,8 @@ classifier = Classifier()
 classifier.clean_data()
 classifier.fit()
 
-# gmail account credentials
-username = os.environ['GMAIL_EMAIL']
-password = os.environ['GMAIL_PASSWORD']
-
-# create IMAP4 class with SSL
-imap = imaplib.IMAP4_SSL('imap.gmail.com')
-
-# authenticate (if fails: <allow less secure apps in gmail account>)
-def authenticate():
-    try:
-        (retcode, capabilities) = imap.login(username, password)
-        print(f'Logged in as {username} at {time.strftime("%H:%M:%S", time.localtime())}.')
-    except imaplib.IMAP4.error:
-        traceback.print_exc()
-
-
-# Close the imap connection
-def close_connection():
-    try:
-        imap.close()
-        imap.logout()
-        print('Closed the connection.')
-    except imaplib.IMAP4.error:
-        print('Error: Connection failed to close.')
-
+# setup imap session
+imapManager = ImapManager(os.environ['GMAIL_EMAIL'], os.environ['GMAIL_PASSWORD'])
 
 # Loop through all emails starting from most recent
 # Using receipt classifier, determine if is valid receipt
@@ -51,7 +25,7 @@ def check_mail():
     count = 0
     print(f'Checking mail for receipts for the next {emailsToCheck} emails..')
 
-    status, messages = imap.select('INBOX')
+    status, messages = imapManager.imap.select('INBOX')
     if status != 'OK': exit('Incorrect mail box')
 
     for i in range(int(messages[0]), 1, -1):
@@ -59,7 +33,7 @@ def check_mail():
             return
         count += 1
 
-        res, msg = imap.fetch(str(i), '(RFC822)')
+        res, msg = imapManager.imap.fetch(str(i), '(RFC822)')
         for response in msg:
             if isinstance(response, tuple):
                 msg = email.message_from_bytes(response[1])
@@ -93,7 +67,7 @@ def check_mail():
                 # # Delete from inbox
                 # typ, data = imap.store(num, '+FLAGS', '\\Deleted')
 
-    close_connection()
+    imapManager.close_connection()
 
 
 # If determined as receipt, forward email to amazon {use correct 'from' address}
@@ -102,5 +76,5 @@ def forward_email(email):
 
 
 spreadsheet = Spreadsheet('AmazonReceipts', 'Sheet1').sheet
-authenticate()
+imapManager.authenticate()
 check_mail()
